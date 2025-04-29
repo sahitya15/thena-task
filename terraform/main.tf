d2e90f877102ae09935a1fec530322bebb16980b
Sahitya Shrivastava <sahityashrivastava@Sahityas-MacBook-Air.local>
2025-04-29 10:57:49 +0530

    add s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "thena-task-bucket"
+    key            = "ephemeral-environments/${var.app_name}/terraform.tfstate"
+    region         = "ap-south-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
